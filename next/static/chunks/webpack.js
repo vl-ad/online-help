253cd602261f379f58158379bf2c212450ecbaf6
vkulakov <kulakov.vladyslav@wdc.com>
2021-03-11 14:34:23 +0200

    .nojekyll

--- next/static/chunks/webpack.js
+++ next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0401299381b024e2092e";
+/******/ 	var hotCurrentHash = "2247c19285dc5ecc158c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
