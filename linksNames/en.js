aa53ffeb72580e89f235cbca5181bbd97b886060
vkulakov <kulakov.vladyslav@wdc.com>
2021-03-17 13:47:02 +0200

    Files

--- linksNames/en.js
+++ linksNames/en.js
@@ -1,54 +1,422 @@
 link({
-  "c2569626.html": "General",
-  "c2569659.html": "Software Overview",
-  "c2572017.html": "How the EdgeRover App Works",
-  "c2569816.html": "Minimum System Requirements",
-  "c2569851.html": "Using the EdgeRover App",
-  "c2556528.html": "Browse",
-  "c2569665.html": "Widgets",
-  "c2569702.html": "Categories",
-  "c2569705.html": "Sources",
-  "c2569858.html": "Collections",
-  "c2556478.html": "Search",
-  "c2556481.html": "Searching for Content",
-  "c2556484.html": "Working with Search Results",
-  "c2556307.html": "Sources",
-  "c2556309.html": "Add External Storage",
-  "c2556315.html": "Add Social",
-  "c2556313.html": "Add Cloud",
-  "c2572184.html": "Manage Devices",
-  "c2556435.html": "Catalog",
-  "c2556443.html": "Catalog Status",
-  "c2556458.html": "What Should I Catalog?",
-  "c2569869.html": "Single Item View",
-  "c2569917.html": "Open",
-  "c2569920.html": "Show Location",
-  "c2569923.html": "Add to Collection",
-  "c2569926.html": "Fullscreen",
-  "c2569929.html": "More Options",
-  "c2569646.html": "Settings",
-  "c2569886.html": "Account Settings",
-  "c2569890.html": "Language",
-  "c2569893.html": "App Settings",
-  "c2569896.html": "Theme",
-  "c2569899.html": "Privacy",
-  "c2569902.html": "Help ",
-  "c2569905.html": "What's New",
-  "c2569908.html": "Legal",
-  "c2569911.html": "Product Tour",
-  "c2569652.html": "Help and Support",
-  "c2572034.html": "Helpful Resources",
-  "c2572104.html": "Frequently Asked Questions (FAQ)",
-  "c2573296.html": "Why is the cataloging process taking so long?",
-  "c2573306.html": "Why has the cataloging process paused?",
-  "c2573309.html": "Why does my computer slow down when the EdgeRover app is running?",
-  "c2572125.html": "Why is the EdgeRover app using large amounts of internal storage space?",
-  "c2572128.html": "Why are some of my videos and audio files not playing?",
-  "c2572131.html": "How do I update the EdgeRover app to the latest version?",
-  "c2573312.html": "Will my files be stored in the cloud?",
-  "c2573333.html": "Which storage devices can I use with the EdgeRover app?",
-  "c2573332.html": "How much do these services and features cost?",
-  "c2573334.html": "Does the EdgeRover app work with other products and devices?",
-  "c2573341.html": "Can I search and browse my content when the storage device is not connected?",
-  "c2573344.html": "Can I delete files from my storage devices after I have cataloged them?"
+  "linksNames": {
+    "c2569626.html": "General",
+    "c2569659.html": "Software Overview",
+    "c2572017.html": "How the EdgeRover App Works",
+    "c2569816.html": "Minimum System Requirements",
+    "c2569851.html": "Using the EdgeRover App",
+    "c2556528.html": "Browse",
+    "c2569665.html": "Widgets",
+    "c2569702.html": "Categories",
+    "c2569705.html": "Sources",
+    "c2569858.html": "Collections",
+    "c2556478.html": "Search",
+    "c2556481.html": "Searching for Content",
+    "c2556484.html": "Working with Search Results",
+    "c2556307.html": "Sources",
+    "c2556309.html": "Add External Storage",
+    "c2556315.html": "Add Social",
+    "c2556313.html": "Add Cloud",
+    "c2572184.html": "Manage Devices",
+    "c2556435.html": "Catalog",
+    "c2556443.html": "Catalog Status",
+    "c2556458.html": "What Should I Catalog?",
+    "c2569869.html": "Single Item View",
+    "c2569917.html": "Open",
+    "c2569920.html": "Show Location",
+    "c2569923.html": "Add to Collection",
+    "c2569926.html": "Fullscreen",
+    "c2569929.html": "More Options",
+    "c2569646.html": "Settings",
+    "c2569886.html": "Account Settings",
+    "c2569890.html": "Language",
+    "c2569893.html": "App Settings",
+    "c2569896.html": "Theme",
+    "c2569899.html": "Privacy",
+    "c2569902.html": "Help ",
+    "c2569905.html": "What's New",
+    "c2569908.html": "Legal",
+    "c2569911.html": "Product Tour",
+    "c2569652.html": "Help and Support",
+    "c2572034.html": "Helpful Resources",
+    "c2572104.html": "Frequently Asked Questions (FAQ)",
+    "c2573296.html": "Why is the cataloging process taking so long?",
+    "c2573306.html": "Why has the cataloging process paused?",
+    "c2573309.html": "Why does my computer slow down when the EdgeRover app is running?",
+    "c2572125.html": "Why is the EdgeRover app using large amounts of internal storage space?",
+    "c2572128.html": "Why are some of my videos and audio files not playing?",
+    "c2572131.html": "How do I update the EdgeRover app to the latest version?",
+    "c2573312.html": "Will my files be stored in the cloud?",
+    "c2573333.html": "Which storage devices can I use with the EdgeRover app?",
+    "c2573332.html": "How much do these services and features cost?",
+    "c2573334.html": "Does the EdgeRover app work with other products and devices?",
+    "c2573341.html": "Can I search and browse my content when the storage device is not connected?",
+    "c2573344.html": "Can I delete files from my storage devices after I have cataloged them?"
+  },
+  "links": [
+    {
+      "tagName": "a",
+      "link": "c2569626.html",
+      "name": "General",
+      "parentLiId": null,
+      "id": "cc7bdb20-bbc4-4f76-b6d3-4d2a99496a9c"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569659.html",
+      "name": "Software Overview",
+      "parentLiId": "cc7bdb20-bbc4-4f76-b6d3-4d2a99496a9c",
+      "id": "db4b5c2a-3b57-4ed4-9f46-27f64b3aeb35"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572017.html",
+      "name": "How the EdgeRover App Works",
+      "parentLiId": "cc7bdb20-bbc4-4f76-b6d3-4d2a99496a9c",
+      "id": "5ecce900-8e6a-41d2-9bfc-cc59c9ba05e4"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569816.html",
+      "name": "Minimum System Requirements",
+      "parentLiId": "cc7bdb20-bbc4-4f76-b6d3-4d2a99496a9c",
+      "id": "73c9a4ea-7f92-4ffd-9a83-9b397f12685a"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569851.html",
+      "name": "Using the EdgeRover App",
+      "parentLiId": null,
+      "id": "499b795f-8f9f-404e-b9ae-d0e28da37944"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556528.html",
+      "name": "Browse",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "b367ec92-399e-43cf-8866-ec00d523dc0a"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569665.html",
+      "name": "Widgets",
+      "parentLiId": "b367ec92-399e-43cf-8866-ec00d523dc0a",
+      "id": "4a74d4e9-32a8-4b64-8b89-ecb7503c4fbc"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569702.html",
+      "name": "Categories",
+      "parentLiId": "b367ec92-399e-43cf-8866-ec00d523dc0a",
+      "id": "e940d1f4-9b59-4cd1-94cd-b23b2b474c9f"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569705.html",
+      "name": "Sources",
+      "parentLiId": "b367ec92-399e-43cf-8866-ec00d523dc0a",
+      "id": "72b7c05b-4dd5-4815-ac85-9f96f6fc86e8"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569858.html",
+      "name": "Collections",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "4230eab1-7263-4cbf-94cd-40a9ba6c7667"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556478.html",
+      "name": "Search",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "226f705d-6949-4269-840a-5379463177bd"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556481.html",
+      "name": "Searching for Content",
+      "parentLiId": "226f705d-6949-4269-840a-5379463177bd",
+      "id": "6ec9ac51-5944-4e44-a7c1-9850fad61432"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556484.html",
+      "name": "Working with Search Results",
+      "parentLiId": "226f705d-6949-4269-840a-5379463177bd",
+      "id": "f1d7a649-76f5-4105-b3a0-80dddba9ef49"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556307.html",
+      "name": "Sources",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "9fee7b8f-0bed-4284-a3c8-7fb824238e0b"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556309.html",
+      "name": "Add External Storage",
+      "parentLiId": "9fee7b8f-0bed-4284-a3c8-7fb824238e0b",
+      "id": "d72ee197-c204-4268-b102-09de0de353b0"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556315.html",
+      "name": "Add Social",
+      "parentLiId": "9fee7b8f-0bed-4284-a3c8-7fb824238e0b",
+      "id": "4c905c2d-7874-4fb5-a256-5a704c9c4998"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556313.html",
+      "name": "Add Cloud",
+      "parentLiId": "9fee7b8f-0bed-4284-a3c8-7fb824238e0b",
+      "id": "6cc0c4b1-05af-4f7d-927b-8bf7bb16c734"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572184.html",
+      "name": "Manage Devices",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "7c33ee8e-4502-43d5-bba7-6ec31a1d0be1"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556435.html",
+      "name": "Catalog",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "04202a5e-b563-4e7e-ba7a-46f51b441380"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556443.html",
+      "name": "Catalog Status",
+      "parentLiId": "04202a5e-b563-4e7e-ba7a-46f51b441380",
+      "id": "976971ab-1fea-4805-969b-f9300a29dd8b"
+    },
+    {
+      "tagName": "a",
+      "link": "c2556458.html",
+      "name": "What Should I Catalog?",
+      "parentLiId": "04202a5e-b563-4e7e-ba7a-46f51b441380",
+      "id": "349003f7-a451-41ee-8170-d1f3236f7106"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569869.html",
+      "name": "Single Item View",
+      "parentLiId": "499b795f-8f9f-404e-b9ae-d0e28da37944",
+      "id": "d750cf66-7ec5-4e1f-9564-18923b143934"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569917.html",
+      "name": "Open",
+      "parentLiId": "d750cf66-7ec5-4e1f-9564-18923b143934",
+      "id": "a3698e01-5134-477a-b917-86ffafa3d4b9"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569920.html",
+      "name": "Show Location",
+      "parentLiId": "d750cf66-7ec5-4e1f-9564-18923b143934",
+      "id": "5c0a71b9-2dfe-407a-856b-1b219af42d2f"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569923.html",
+      "name": "Add to Collection",
+      "parentLiId": "d750cf66-7ec5-4e1f-9564-18923b143934",
+      "id": "610cee77-c451-4160-8664-53685a959ec4"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569926.html",
+      "name": "Fullscreen",
+      "parentLiId": "d750cf66-7ec5-4e1f-9564-18923b143934",
+      "id": "4760279e-46fc-41c3-becd-1e1954790685"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569929.html",
+      "name": "More Options",
+      "parentLiId": "d750cf66-7ec5-4e1f-9564-18923b143934",
+      "id": "2a4182ae-9de0-4356-adb7-a0df0e4b36d6"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569646.html",
+      "name": "Settings",
+      "parentLiId": null,
+      "id": "8fa1413a-415a-40e7-a116-c5875f7b9f02"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569886.html",
+      "name": "Account Settings",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "8e7dd9d8-f77e-4381-a429-f5f681882d94"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569890.html",
+      "name": "Language",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "f90a078e-96c2-405f-abf4-ac703da64726"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569893.html",
+      "name": "App Settings",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "8bb7fe67-adc5-4278-a854-c514b5a2d131"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569896.html",
+      "name": "Theme",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "9b744df5-dbfb-43dd-9a05-f8bf5af8cf09"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569899.html",
+      "name": "Privacy",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "52eaf4a3-c07c-4f01-b5bf-03744d754594"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569902.html",
+      "name": "Help ",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "b62e2e27-6f64-4999-b6b5-82008b60599f"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569905.html",
+      "name": "What's New",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "34726a5f-f980-414a-b32c-2eb265a22cf5"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569908.html",
+      "name": "Legal",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "ae3eac4f-6ec6-4962-8503-fd8ab0f6bcd4"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569911.html",
+      "name": "Product Tour",
+      "parentLiId": "8fa1413a-415a-40e7-a116-c5875f7b9f02",
+      "id": "ee888a94-1967-4da2-bd23-50b9a154a19c"
+    },
+    {
+      "tagName": "a",
+      "link": "c2569652.html",
+      "name": "Help and Support",
+      "parentLiId": null,
+      "id": "3dcd1e9c-2751-4511-94b3-48cc95b64602"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572034.html",
+      "name": "Helpful Resources",
+      "parentLiId": "3dcd1e9c-2751-4511-94b3-48cc95b64602",
+      "id": "94f3b9bf-8774-4898-b312-58cc6a91e307"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572104.html",
+      "name": "Frequently Asked Questions (FAQ)",
+      "parentLiId": "3dcd1e9c-2751-4511-94b3-48cc95b64602",
+      "id": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573296.html",
+      "name": "Why is the cataloging process taking so long?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "3b07de35-54ee-4fd9-bd02-7c2df3fbc5a5"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573306.html",
+      "name": "Why has the cataloging process paused?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "74b4aee3-655d-4865-9c13-8de21b08ef5e"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573309.html",
+      "name": "Why does my computer slow down when the EdgeRover app is running?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "c6ee8065-8bde-4b20-9bf7-dc9732e539a8"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572125.html",
+      "name": "Why is the EdgeRover app using large amounts of internal storage space?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "1c850449-38ef-4bb1-beb4-3387a824cb0a"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572128.html",
+      "name": "Why are some of my videos and audio files not playing?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "05919eda-7c5d-499a-95d6-296370e6f83e"
+    },
+    {
+      "tagName": "a",
+      "link": "c2572131.html",
+      "name": "How do I update the EdgeRover app to the latest version?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "d1951436-e923-43bf-b864-8e07ef7ff92f"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573312.html",
+      "name": "Will my files be stored in the cloud?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "66d4a54c-6aed-4a72-856e-23a5c6ba9c3d"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573333.html",
+      "name": "Which storage devices can I use with the EdgeRover app?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "5cfb92a4-b734-40f1-acc9-a86a9e52574c"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573332.html",
+      "name": "How much do these services and features cost?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "e8502480-4e1d-4c60-8e71-961f725bc2cf"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573334.html",
+      "name": "Does the EdgeRover app work with other products and devices?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "d45d7bae-fa6b-40f0-ae6c-697340412c10"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573341.html",
+      "name": "Can I search and browse my content when the storage device is not connected?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "643f8ac5-bb2c-4e21-aff9-84252d496808"
+    },
+    {
+      "tagName": "a",
+      "link": "c2573344.html",
+      "name": "Can I delete files from my storage devices after I have cataloged them?",
+      "parentLiId": "8da51b8f-0f2f-4bfe-aff1-f3ef343ff0a2",
+      "id": "c761c2dc-bc8a-4150-b3c7-d43beab18a87"
+    }
+  ]
 })
--- next/static/chunks/webpack.js
+++ next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e32de5a614cd8a5c7592";
+/******/ 	var hotCurrentHash = "2c93ef2409e267ec5527";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
